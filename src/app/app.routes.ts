7a321ad467db87bc42683e8a700189479352f7a3
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-08-27 11:00:34 +0200

    redirect vers admin dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
+import { adminGuard } from './guards/admin.guard';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { DonorDashboardComponent } from './components/donor-dashboard/donor-dashboard.component';
 
 export const routes: Routes = [
     { path: '', component: DashboardComponent }, 
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent},
     { path: 'dashboard', component: DashboardComponent },
+    { path: 'admin-dashboard', canActivate:[adminGuard],component : AdminDashboardComponent },
+    { path: 'donor-dashboard', component: DonorDashboardComponent},
 
     { path: '**', redirectTo: 'login' }
 ];
--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../../core/auth/auth.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -17,12 +18,15 @@ export class RegisterComponent {
     password: ''
   };
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router : Router) {}
 
   onSubmit() {
     console.log("Submit clicked", this.form);
     this.authService.registerDonor(this.form).subscribe({
-      next: res => console.log('Success', res),
+      next: res => {
+        console.log('Success', res);
+      this.router.navigate(['/login']);
+      },
       error: err => console.error('Error', err)
     });
   }
